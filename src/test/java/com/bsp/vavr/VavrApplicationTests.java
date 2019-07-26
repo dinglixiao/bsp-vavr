3f5386faba621a63b6c57d1a6e03eca3a8a9ed6e
dinglixiao <1713716789@qq.com>
2019-07-26 12:07:59 +0800

    Initial commit

---
+++ src/test/java/com/bsp/vavr/VavrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bsp.vavr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VavrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
