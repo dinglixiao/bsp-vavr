205d4ad3b009b97dc74a06d7c732860154ee9f3a
dinglixiao <1713716789@qq.com>
2019-09-11 14:49:25 +0800

    file changes

--- src/main/java/com/bsp/vavr/Test.java
+++ src/main/java/com/bsp/vavr/Test.java
@@ -1,8 +1,17 @@
 package com.bsp.vavr;
 
+import io.vavr.Function2;
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import io.vavr.collection.Stream;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Test {
 
     public static void main(String[] args) {
 
+        Function2<Integer, Integer, Integer> sum = (a, b) -> a + b;
     }
 }
